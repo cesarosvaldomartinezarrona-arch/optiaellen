a797c70bab3870b9076a2fc0a8033879609a1b5e
Cesar Osvaldo Martinez Arrona <cesarosvaldomartinezarrona@gmail.com>
2026-08-23 13:52:11 -0600

    fix: basename dinamico para dev y production

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
+const basename = import.meta.env.BASE_URL === '/' ? '' : import.meta.env.BASE_URL
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/optiaellen">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
