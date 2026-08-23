819cb25dc3cd66e8261efdc6a92312e3c37bba75
Cesar Osvaldo Martinez Arrona <cesarosvaldomartinezarrona@gmail.com>
2026-08-23 13:50:38 -0600

    fix: SPA routing en GitHub Pages
    - BrowserRouter basename=/optiaellen
    - 404.html redirect a index.html
    - Eliminado spa-redirect.js innecesario

--- public/spa-redirect.js
+++
@@ -1,8 +0,0 @@
-(function(l) {
-  if (l.search[1] === '/') {
-    document.onready = function() {
-      var newLink = l.pathname.slice(0, -1) + l.search.slice(1) + l.hash;
-      window.history.replaceState(null, null, newLink);
-    };
-  }
-}(window.location))
